56b5a9d05faa7ff632dc6c10f8ef0f0cfebf6f11
hamadomer <158159878+hamadomer@users.noreply.github.com>
2024-06-03 14:44:07 +0200

    add files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        PdfHandler pdf = new PdfHandler();
+        pdf.pdfToString("Attachments\\exo2_format1.pdf");
+    }
+}
