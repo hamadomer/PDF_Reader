d260d7e1d3ab6a60cb72c0c60fdb047cab0a8a68
hamadomer <158159878+hamadomer@users.noreply.github.com>
2024-06-03 14:43:39 +0200

    Delete PDF_Reader directory

--- PDF_Reader/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        PdfHandler pdf = new PdfHandler();
-        pdf.pdfToString("Attachments\\exo2_format1.pdf");
-    }
-}
